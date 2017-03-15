05a3ed896ffb21fd0f9c3ccbc86ebf6823ae8890
syahrulutomo <irullvandalis@gmail.com>
2017-03-15 22:10:34 +0700

    initial commit

---
+++ src/main/java/belajar/restfull/User.java
@@ -0,0 +1,34 @@
+
+package belajar.restfull;
+
+/**
+ *
+ * @author syahrul
+ */
+public class User {
+    
+    private long id;
+    private String name;
+    private String email;
+
+    public User(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    
+    
+}
---
+++ src/main/java/belajar/restfull/UserController.java
@@ -0,0 +1,20 @@
+package belajar.restfull;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+    private static final AtomicLong counter = new AtomicLong();
+    
+    @RequestMapping("/user")
+    public Object user(@RequestParam(value="username" ,defaultValue="user001") String username)
+    {
+        long id = counter.incrementAndGet();
+        User u = new User(id,username,"irull@yahoo.com");
+        return u;
+    }
+}
